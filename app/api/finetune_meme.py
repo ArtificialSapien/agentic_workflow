63053b3749444d00b26d650339181f1615290d06
Aron Horvath <arcsi1989@gmail.com>
2024-12-08 09:20:26 +0100

    Add place holder for meme generator

---
+++ app/api/finetune_meme.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from langgraph.graph import StateGraph, START, END
+from langchain_openai import AzureChatOpenAI
+
+from app.schemas import response
+from app.schemas import request
+from app.agents.agent_state import AgentState
+
+router = APIRouter()
+
+llm = AzureChatOpenAI(deployment_name="gpt-4o-mini")
+
+
+@router.post("/finetune_meme/", response_model=response.FineTunedMeme)
+def finetune_meme(request: request.FineTuneRequest):
+    pass  # TODO @Maaz
