7f27ae507df3d69b0f98685a77a3edde9cdd2970
Alexander Mikasch <alexander.mikasch@ruhr-uni-bochum.de>
2024-12-08 22:20:16 +0100

    Integration Static

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 from app.api.generate_post import router as post_router
 from app.api.finetune_text import router as finetunetext_router
@@ -15,6 +16,8 @@ app.add_middleware(
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
 )
+
+app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="static")
 
 app.include_router(post_router, tags=["post_prompt"], prefix="/post_prompt")
 app.include_router(
