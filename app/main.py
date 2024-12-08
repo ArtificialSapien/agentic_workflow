79e3120b7c77caeffb9a34e572b78a7cf7461d1f
Alexander Mikasch <alexander.mikasch@ruhr-uni-bochum.de>
2024-12-08 22:25:47 +0100

    add 8000

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Adjust origin as needed
+    allow_origins=["http://localhost:5173", "http://0.0.0.0:8000"],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
